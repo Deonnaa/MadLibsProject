c77f64e230daae3bf7f23a5978b7f7a2b5101eba
Deonnaa <aponte.deonna@gmail.com>
2023-11-03 09:07:23 -0400

    initial commit

---
+++ src/madlib/MadLib.java
@@ -0,0 +1,70 @@
+package madlib;
+
+import java.util.Scanner;
+
+public class MadLib {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Choose a Number");
+		System.out.println("1:Short Story");
+		System.out.println("2:Long Story");
+		String storyType = sc.nextLine();
+
+		switch (storyType) {
+		case "1":
+			storyMethod();
+			break;
+		case "2":
+			longStoryMethod();
+
+			break;
+		default:
+			System.err.println("Enter 1 or 2.");
+			System.out.println(" ");
+		}
+	}
+
+	public static void storyMethod() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter a noun: ");
+		String noun = sc.nextLine();
+
+		System.out.print("Enter an adjective: ");
+		String adjective = sc.nextLine();
+
+		System.out.print("Enter a plural noun: ");
+		String nounPlural = sc.nextLine();
+
+		System.out.print("Enter a verb: ");
+		String verb = sc.nextLine();
+
+		System.out.println("You go to a " + noun + " and see " + adjective + " " + nounPlural + ". ");
+		System.out.println("You decide to " + verb + " them... ");
+	}
+
+	public static void longStoryMethod() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter a noun: ");
+		String noun = sc.nextLine();
+
+		System.out.print("Enter a adjective: ");
+		String adjective = sc.nextLine();
+
+		System.out.print("Enter a plural noun: ");
+		String nounPlural = sc.nextLine();
+
+		System.out.print("Enter a verb: ");
+		String verb = sc.nextLine();
+
+		System.out.println("You go to a " + noun + " and see " + adjective + " " + nounPlural + ". ");
+		System.out.println("You decide to " + verb + " them... ");
+		System.out.println("You go to a " + noun + " and see " + adjective + " " + nounPlural + ". ");
+		System.out.println("You decide to " + verb + " them... ");
+		System.out.println("You go to a " + noun + " and see " + adjective + " " + nounPlural + ". ");
+		System.out.println("You decide to " + verb + " them... ");
+	}
+}
